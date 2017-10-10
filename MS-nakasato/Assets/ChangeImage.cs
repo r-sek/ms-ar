ba8125cf746d9f7e3ace09a9bb8878e4424d5212
kuma-corp <narutakki@gmail.com>
2017-10-10 15:08:11 +0900

    Unityバージョン修正

---
+++ MS-nakasato/Assets/ChangeImage.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class changeimage : MonoBehaviour {
+	public string BASE_TEXTURE;
+	public int i = 0;
+
+	public Sprite image2;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+	public void chaged(){
+		SpriteRenderer rende = gameObject.GetComponent<SpriteRenderer> ();
+		rende.sprite = image2;
+	}
+	public void texturechange(){
+		i++;
+		if (i > 5) {
+			i = 1;
+		}
+		BASE_TEXTURE = Application.temporaryCachePath;
+		GameObject cube = GameObject.FindGameObjectWithTag ("test");
+		//Texture[] textures = Resources.LoadAll<Texture>(BASE_TEXTURE);
+		Texture2D textures = new Texture2D(0,0);
+		textures.LoadImage(LoadBin(BASE_TEXTURE + "/" + i + ".png"));
+		Renderer renderer = cube.GetComponent<Renderer> ();
+		renderer.material.mainTexture = textures;
+	}
+	public void textureback(){
+		i--;
+		if (i <= 1) {
+			i = 5;
+		}
+		BASE_TEXTURE = Application.temporaryCachePath;
+		GameObject cube = GameObject.FindGameObjectWithTag ("test");
+		//Texture[] textures = Resources.LoadAll<Texture>(BASE_TEXTURE);
+		Texture2D textures = new Texture2D(0,0);
+		textures.LoadImage (LoadBin (BASE_TEXTURE + "/" + i + ".png"));
+		Renderer renderer = cube.GetComponent<Renderer> ();
+		renderer.material.mainTexture = textures;
+
+	}
+	byte[] LoadBin(string path){
+		FileStream fs = new FileStream(path, FileMode.Open);
+		BinaryReader br = new BinaryReader(fs);
+		byte[] buf = br.ReadBytes( (int)br.BaseStream.Length);
+		br.Close();
+		return buf;
+	}
+}
---
+++ MS-nakasato/Assets/DownloadImages.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+using System.IO;
+
+public class downloadScript : MonoBehaviour {
+
+	//private string path = "Assets/Resources/textures/ai.jpg";
+	// Use this for initialization
+	// index
+	private string cae ="";
+	const string serve = "http://superkuma.net/textures/";
+
+	void Start () {
+		StartCoroutine (GetFile ());
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//Texture tex = readByBinary (readPngFile(path));
+		//Renderer renderer = this.GetComponent<Renderer>();
+		//renderer.material.mainTexture = tex;
+		//print ("a");
+		//			if(i < 5){
+		//				i++;
+		//			}else{
+		//				i = 1;
+		//			}
+		//			string path = serve + i + ".png";
+		//			StartCoroutine(GetFile(path));
+	}
+
+	public Texture readByBinary(byte[] bytes) {
+		Texture2D texture = new Texture2D (1, 1);
+		texture.LoadImage (bytes);
+		return texture;
+	}
+
+	public byte[] readPngFile(string path) {
+		using (FileStream fileStream = new FileStream (path, FileMode.Open, FileAccess.Read)) {
+			BinaryReader bin = new BinaryReader (fileStream);
+			byte[] values = bin.ReadBytes ((int)bin.BaseStream.Length);
+			bin.Close ();
+			return values;
+		}
+	}
+
+	IEnumerator GetFile(){
+		for (int i = 1; i < 6; i++) {
+			string path = serve + i +".png";
+			using (UnityWebRequest www = UnityWebRequest.Get (path)) {
+				yield return www.Send ();
+				if (www.isError) {
+				} else {
+					//Texture tex = readByBinary (www.downloadHandler.data);
+					//Renderer renderer = this.GetComponent<Renderer>();
+					//renderer.material.mainTexture = tex;
+					print (Application.temporaryCachePath + "/" + i + ".png");
+
+					cae = Application.temporaryCachePath;
+					File.WriteAllBytes (cae + "/" + i + ".png", www.downloadHandler.data);
+				}
+			}
+		}
+	}
+}
