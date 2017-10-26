ce522d17e5a9ae0457a074486668c7578f5171ed
pasco3596 <sa23974@pf7.so-net.ne.jp>
2017-10-26 10:10:30 +0900

    整え

---
+++ MS-nakasato/Assets/Scripts/Utilities.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public static class Utilities {
+
+    public static byte[] LoadbinaryBytes(string path) {
+        using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read)) {
+            using (var bin = new BinaryReader(fs)) {
+                return bin.ReadBytes((int) bin.BaseStream.Length);
+            }
+        }
+    }    
+    public static Sprite GetSpriteFromTexture2D(Texture2D t2D) {
+        Sprite sprite = null;
+        if (t2D) {
+            sprite = Sprite.Create(t2D, new Rect(0, 0, t2D.width, t2D.height), Vector2.zero);
+        }
+        return sprite;
+    }
+}
