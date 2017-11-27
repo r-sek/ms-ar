336b2e831a3ee23c1773b14694663f79a37aa0ce
kuma-corp <narutakki@gmail.com>
2017-11-27 13:57:29 +0900

    Animation実装（プログラム処理とテスト処理）

--- Assets/Scripts/PostData.cs
+++ Assets/Scripts/PostData.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -134,4 +134,4 @@ public class PostData : MonoBehaviour {
         }
     }
 #endif
-}
+}*/
---
+++ Assets/particle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particle : MonoBehaviour {
+	private ParticleSystem ps;
+	// Use this for initialization
+	void Start () {
+		ps = this.GetComponent<ParticleSystem> ();
+		ps.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
