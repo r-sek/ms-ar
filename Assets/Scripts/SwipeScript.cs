7e6b4be35a69ba60548e0c5c917d297a4a8160ba
kuma-corp <narutakki@gmail.com>
2017-11-28 15:41:26 +0900

    画面遷移用アニメーション追加

--- Assets/Scripts/SwipeScript.cs
+++ Assets/Scripts/SwipeScript.cs
@@ -1,25 +1,25 @@
 ﻿using UniRx;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 [RequireComponent(typeof(SwipeGesture))]
 public class SwipeScript : MonoBehaviour {
     private SwipeGesture swipeGesture;
-
+	public Camera maincamera;
+	public CanvasRenderer renderer;
+	private AsyncOperation async;
     // Use this for initialization
     void Start() {
-    }
+		renderer.SetAlpha (0);
+	}
 
     void OnEnable() {
         swipeGesture = GetComponent<SwipeGesture>();
         swipeGesture.OnSwipeRight
             .Subscribe(_ => {
                 Debug.unityLogger.Log("swipe", "右");
-                #if UNITY_IOS
-					SceneManager.LoadScene("iOSUploadScene");
-				#else
-                    SceneManager.LoadScene("UploadScene");
-                #endif
+				StartCoroutine(ChangeScene());
             });
         swipeGesture.OnSwipeLeft
             .Subscribe(_ => { Debug.unityLogger.Log("swipe", "左"); });
@@ -28,4 +28,31 @@ public class SwipeScript : MonoBehaviour {
         swipeGesture.OnSwipeUp
             .Subscribe(_ => { Debug.unityLogger.Log("swipe", "上"); });
     }
+
+	private IEnumerator ChangeScene(){
+		int i = 0;
+		#if UNITY_IOS
+		async = SceneManager.LoadSceneAsync("iOSUploadScene");
+		while(!async.isDone){
+			yield return new WaitForSeconds(0.5f);
+			if(i%2 == 0){
+				renderer.SetAlpha(0);	
+			}else{
+				renderer.SetAlpha(1);
+			}
+			i++;
+		}
+		#else
+		async = SceneManager.LoadSceneAsync("UploadScene");
+		while(!async.isDone){
+			yield return new WaitForSeconds(0.5f);
+			if(i%2 == 0){
+				renderer.SetAlpha(0);
+			}else{
+				renderer.SetAlpha(1);
+			}
+			i++;
+		}
+		#endif
+	}
 }
