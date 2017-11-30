466b9d33c625cd4eda16a108ade1a1a5cd7a9262
kuma-corp <narutakki@gmail.com>
2017-11-30 14:08:31 +0900

    コミット

--- Assets/Scripts/PostData.cs
+++ Assets/Scripts/PostData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/*
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -138,3 +139,4 @@ public class PostData : MonoBehaviour {
     }
 #endif
 }
+*/
