de009c49d6bb407598ea9a3155941b927fe9c4fe
kuma-corp <narutakki@gmail.com>
2017-12-07 02:24:13 +0900

    プレゼン用ターゲット生成

---
+++ Assets/localTrackable.cs
@@ -0,0 +1,96 @@
+﻿/*==============================================================================
+Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Protected under copyright and other laws.
+==============================================================================*/
+
+using UnityEngine;
+
+namespace Vuforia
+{
+	/// <summary>
+	/// A custom handler that implements the ITrackableEventHandler interface.
+	/// </summary>
+
+	public class localTrackable : MonoBehaviour,
+	ITrackableEventHandler
+	{
+		public setImage setimage;
+		public MeshRenderer text;
+		public SpriteRenderer sprite;
+		public MeshRenderer board;
+
+
+		#region PRIVATE_MEMBER_VARIABLES
+
+		private TrackableBehaviour mTrackableBehaviour;
+
+		#endregion // PRIVATE_MEMBER_VARIABLES
+
+
+
+		#region UNTIY_MONOBEHAVIOUR_METHODS
+
+		void Start()
+		{
+			mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+			if (mTrackableBehaviour)
+			{
+				mTrackableBehaviour.RegisterTrackableEventHandler(this);
+			}
+		}
+
+		#endregion // UNTIY_MONOBEHAVIOUR_METHODS
+
+
+
+		#region PUBLIC_METHODS
+
+		/// <summary>
+		/// Implementation of the ITrackableEventHandler function called when the
+		/// tracking state changes.
+		/// </summary>
+		public void OnTrackableStateChanged(
+			TrackableBehaviour.Status previousStatus,
+			TrackableBehaviour.Status newStatus)
+		{
+			if (newStatus == TrackableBehaviour.Status.DETECTED ||
+				newStatus == TrackableBehaviour.Status.TRACKED ||
+				newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+			{
+				OnTrackingFound();
+			}
+			else
+			{
+				OnTrackingLost();
+			}
+		}
+
+		#endregion // PUBLIC_METHODS
+
+
+
+		#region PRIVATE_METHODS
+
+
+		private void OnTrackingFound()
+		{
+			setimage.Texturechange ();
+			text.enabled = true;
+			sprite.enabled = true;
+			board.enabled = true;
+			Debug.Log ("FoundMarker4");
+		}
+
+
+		private void OnTrackingLost()
+		{
+			text.enabled = false;
+			sprite.enabled = false;
+			board.enabled = false;
+			Debug.Log ("LostMarker4");
+		}
+
+		#endregion // PRIVATE_METHODS
+	}
+}
